9c5067d8462bfc7b1cee8c1b4dff406d56ac066c
Mavissz <maria.v.souza30@aluno.senai.br>
2024-09-20 14:49:22 -0300

    primeiro

---
+++ CalculadoraEletricaC-main/utils/enuns.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class EnumBandeira(Enum):
+    bandeira = 'Bandeira'
+
+class EnumDependencia(Enum):
+    dependencia = 'Dependencia'
+
+class EnumDispositivo(Enum):
+    dispositivo = 'Dispositivo'
+
+class EnumTipoConsumidor(Enum):
+    tipo_consumidor = 'Tipo de Consumidor'
+
+class EnumTipoDispositivo(Enum):
+    tipo_dispositivo = 'Tipo de Dispositivo'
+
+class EnumUnidadeConsumidora(Enum):
+    unidade_consumidora = 'Unidade Consumidora'
